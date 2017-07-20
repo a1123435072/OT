e367cc4fc770a73228e6f8a9eed459587a9f0378
HUAQING\yangg <1123435072@.com>
2017-07-20 19:36:21 +0800

    保存代码

---
+++ app/src/main/java/cn/zzu/takeout/presenter/MainActivityPresenter.java
@@ -0,0 +1,21 @@
+package cn.zzu.takeout.presenter;
+
+import cn.zzu.takeout.MainActivity;
+
+/**
+ * Created by yangg on 2017/7/20.
+ * 跟登录相关的业务处理
+ */
+
+public class MainActivityPresenter {
+
+    private MainActivity activity;
+
+    public MainActivityPresenter(MainActivity activity) {
+        this.activity = activity;
+    }
+
+    public void login(String username , String password){
+
+    }
+}
